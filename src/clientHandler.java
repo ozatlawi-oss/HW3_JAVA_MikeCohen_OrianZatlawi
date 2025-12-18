6db8bf1563db2b7ea8bac52ced3476ad469687e3
mikecohenil <143258797+MikeCohenIL@users.noreply.github.com>
2025-12-18 12:43:39 +0200

    Test

---
+++ src/clientHandler.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
+public class clientHandler extends Thread {
+    private final Socket clientSocket;
+    public clientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+    @Override
+    public void run() {
+        try(
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+                BufferedReader in = new BufferedReader(new InputStreamReader((clientSocket.getInputStream())));
+
+                ){
+
+            String inputLine, outputLine;
+            KnockKnockProtocol kkp = new KnockKnockProtocol();
+
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                if (inputLine.equals("q"))  break;
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+
+            }
+            out.close();
+            in.close();
+
+        }
+        catch (IOException e){
+
+        }
+        finally{
+            try{
+                clientSocket.close();
+            } catch (IOException e) {
+
+            }
+        }
+    }
+}
